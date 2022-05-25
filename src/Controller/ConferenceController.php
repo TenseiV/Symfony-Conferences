8aa46a576a2f1f8ed6ed4e55a21c3015b0e57da6
TenseiV <36626656+TenseiV@users.noreply.github.com>
2022-05-25 19:07:03 +0200

    use akismet api for verify comments post

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -14,6 +14,7 @@ use App\Entity\Comment;
 use App\Form\CommentFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use App\SpamChecker;
 
 class ConferenceController extends AbstractController
 {
@@ -35,7 +36,7 @@ class ConferenceController extends AbstractController
 
 
     #[Route('/conference/{slug}', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository,SpamChecker $spamChecker, string $photoDir): Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
@@ -53,6 +54,15 @@ class ConferenceController extends AbstractController
                             }
 
             $this->entityManager->persist($comment);
+            $context = [
+                        'user_ip' => $request->getClientIp(),
+                        'user_agent' => $request->headers->get('user-agent'),
+                        'referrer' => $request->headers->get('referer'),
+                        'permalink' => $request->getUri(),
+                        ];
+            if (2 === $spamChecker->getSpamScore($comment, $context)) {
+                throw new \RuntimeException('Blatant spam, go away!');
+            }
             $this->entityManager->flush();
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
